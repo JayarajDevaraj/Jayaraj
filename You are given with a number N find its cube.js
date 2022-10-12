de8f609eef818cab7c012f180c8c408c651cedd3
JayarajDevaraj <114243019+JayarajDevaraj@users.noreply.github.com>
2022-10-12 12:48:32 +0530

    Add files via upload

---
+++ You are given with a number N find its cube.js
@@ -0,0 +1,28 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+    const userInput = [];
+    
+    
+inp.on("line", (data) => {
+    
+    userInput.push(data);
+    
+});
+
+inp.on("close", () => {
+    
+ let n = userInput[0];
+ 
+    
+ 
+    let cube=n*n*n;
+ 
+    console.log(cube);   
+    
+    
+});
